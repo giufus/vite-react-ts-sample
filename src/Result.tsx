43b0a3b92439f67505fb7847a9cca21c2d8c45c0
giuseppe.fusacchia <giuseppe.fusacchia@assistdigital.com>
2023-08-15 13:56:34 +0200

    update: css modules

--- src/Result.tsx
+++ src/Result.tsx
@@ -1,13 +1,13 @@
-import './Result.css'
+import s from './Result.module.css'
 
 export function Result(props: any) {
 
 
     return <>
-    <div id='results' className="results">
+    <div id='results' className={s.results}>
         {
             props.results.map((result: any, id: number) => (
-                <div id={'result' + id} className='result' key={id}>
+                <div id={'result' + id} className={s.result} key={id}>
                     <pre><code>{JSON.stringify(result, null, 2)}</code></pre>
                 </div>))
         }
